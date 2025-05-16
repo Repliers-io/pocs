de0376a05958f1389e1364a5eca62e99bb855e83
Milan Somasundaram <sarmilans@gmail.com>
2025-05-16 12:32:29 -0400

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+  ],
+  framework: {
+    name: "@storybook/experimental-nextjs-vite",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      optimizeDeps: {
+        ...config.optimizeDeps,
+        include: [
+          ...(config.optimizeDeps?.include ?? []),
+          "@hookform/resolvers/zod",
+        ],
+      },
+    };
+  },
+};
+export default config;
---
+++ assets/index-CsKiN7zt.js
@@ -0,0 +1,1 @@
+import{r}from"./jsx-runtime-B2VJbz7C.js";import{g as o}from"./index-CoAThXCU.js";import{r as t}from"./index-fny-Lthe.js";var i=r(),s=t();const p=o(s);export{p as h,i as j,s as r};
